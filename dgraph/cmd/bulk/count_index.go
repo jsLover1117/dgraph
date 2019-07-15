jsLover1117/dgraph

commit 039ff199be5f04b53c14170199034817772634f5

Use StreamWriter in bulk loader (#3542)
This PR switched transaction based writes to Badger to StreamWriter and brings in Badger master into vendor.

This PR also refactors bulk loader code as follows:

- Simplify shuffler and reducer code and merge them into one, i.e. reducer.
- Remove shuffler.go file.
- Remove metrics.go file.
- The channel based heap merge was expensive. Switched that with a simple map entries iterator.

With these changes, the 21M dataset now takes 2 mins to load from the original 3 mins.


Changes:
* Simplified shuffler and reducer code. But, encountered an issue where key sorted order changes due to version append in Badger.
* Working code after StreamWriter integration.
* Vendor Badger in, because it contains fixes to StreamWriter.
* Fix build breakages caused by importing Badger.

dgraph/cmd/bulk/count_index.go
@@ -17,10 +17,13 @@
 package bulk
 
 import (
+	"bytes"
 	"sort"
 	"sync"
+	"sync/atomic"
 
 	"github.com/dgraph-io/badger"
+	bpb "github.com/dgraph-io/badger/pb"
 	"github.com/dgraph-io/dgraph/codec"
 	"github.com/dgraph-io/dgraph/posting"
 	"github.com/dgraph-io/dgraph/protos/pb"
@@ -34,8 +37,8 @@ type current struct {
 }
 
 type countIndexer struct {
-	*state
-	db     *badger.DB
+	*reducer
+	writer *badger.StreamWriter
 	cur    current
 	counts map[int][]uint64
 	wg     sync.WaitGroup
@@ -72,8 +75,10 @@ func (c *countIndexer) addUid(rawKey []byte, count int) {
 }
 
 func (c *countIndexer) writeIndex(pred string, rev bool, counts map[int][]uint64) {
-	writer := posting.NewTxnWriter(c.db)
+	defer c.wg.Done()
 
+	streamId := atomic.AddUint32(&c.streamId, 1)
+	list := &bpb.KVList{}
 	for count, uids := range counts {
 		sort.Slice(uids, func(i, j int) bool { return uids[i] < uids[j] })
 
@@ -81,12 +86,20 @@ func (c *countIndexer) writeIndex(pred string, rev bool, counts map[int][]uint64
 		pl.Pack = codec.Encode(uids, 256)
 		data, err := pl.Marshal()
 		x.Check(err)
-		x.Check(writer.SetAt(
-			x.CountKey(pred, uint32(count), rev),
-			data, posting.BitCompletePosting, c.state.writeTs))
+		list.Kv = append(list.Kv, &bpb.KV{
+			Key:      x.CountKey(pred, uint32(count), rev),
+			Value:    data,
+			Meta:     []byte{posting.BitCompletePosting},
+			Version:  c.state.writeTs,
+			StreamId: streamId,
+		})
 	}
-	x.Check(writer.Flush())
-	c.wg.Done()
+	sort.Slice(list.Kv, func(i, j int) bool {
+		return bytes.Compare(list.Kv[i].Key, list.Kv[j].Key) < 0
+	})
+	if err := c.writer.Write(list); err != nil {
+		x.Check(err)
+	}
 }
 
 func (c *countIndexer) wait() {

dgraph/cmd/bulk/loader.go
@@ -52,7 +52,7 @@ type options struct {
 	MapBufSize       int64
 	SkipMapPhase     bool
 	CleanupTmp       bool
-	NumShufflers     int
+	NumReducers      int
 	Version          bool
 	StoreXids        bool
 	ZeroAddr         string
@@ -220,25 +220,15 @@ func (ld *loader) mapStage() {
 }
 
 type shuffleOutput struct {
-	db         *badger.DB
+	writer     *badger.StreamWriter
 	mapEntries []*pb.MapEntry
 }
 
 func (ld *loader) reduceStage() {
 	ld.prog.setPhase(reducePhase)
 
-	shuffleOutputCh := make(chan shuffleOutput, 100)
-	go func() {
-		shuf := shuffler{state: ld.state, output: shuffleOutputCh}
-		shuf.run()
-	}()
-
-	redu := reducer{
-		state:     ld.state,
-		input:     shuffleOutputCh,
-		writesThr: x.NewThrottle(100),
-	}
-	redu.run()
+	r := reducer{state: ld.state}
+	x.Check(r.run())
 }
 
 func (ld *loader) writeSchema() {

dgraph/cmd/bulk/metrics.go
@@ -1,25 +1,0 @@
-/*
- * Copyright 2017-2018 Dgraph Labs, Inc. and Contributors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package bulk
-
-import "expvar"
-
-var (
-	NumBadgerWrites     = expvar.NewInt("dgraph-bulk-loader_badger_writes_pending")
-	NumReducers         = expvar.NewInt("dgraph-bulk-loader_num_reducers_total")
-	NumQueuedReduceJobs = expvar.NewInt("dgraph-bulk-loader_reduce_queue_size")
-)

dgraph/cmd/bulk/reduce.go
@@ -17,47 +17,258 @@
 package bulk
 
 import (
+	"bufio"
 	"bytes"
+	"container/heap"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"log"
+	"os"
 	"sync/atomic"
 
 	"github.com/dgraph-io/badger"
-
+	bo "github.com/dgraph-io/badger/options"
+	bpb "github.com/dgraph-io/badger/pb"
+	"github.com/dgraph-io/badger/y"
 	"github.com/dgraph-io/dgraph/codec"
 	"github.com/dgraph-io/dgraph/posting"
 	"github.com/dgraph-io/dgraph/protos/pb"
 	"github.com/dgraph-io/dgraph/x"
+	"github.com/gogo/protobuf/proto"
 )
 
 type reducer struct {
 	*state
-	input     <-chan shuffleOutput
-	writesThr *x.Throttle
+	streamId uint32
 }
 
-func (r *reducer) run() {
-	thr := x.NewThrottle(r.opt.NumGoroutines)
-	for reduceJob := range r.input {
-		thr.Start()
-		NumReducers.Add(1)
-		NumQueuedReduceJobs.Add(-1)
-		r.writesThr.Start()
-		go func(job shuffleOutput) {
-			r.reduce(job)
-			thr.Done()
-			NumReducers.Add(-1)
-		}(reduceJob)
+func (r *reducer) run() error {
+	dirs := shardDirs(r.opt.TmpDir)
+	x.AssertTrue(len(dirs) == r.opt.ReduceShards)
+	x.AssertTrue(len(r.opt.shardOutputDirs) == r.opt.ReduceShards)
+
+	thr := y.NewThrottle(r.opt.NumReducers)
+	for i := 0; i < r.opt.ReduceShards; i++ {
+		if err := thr.Do(); err != nil {
+			return err
+		}
+		go func(shardId int, db *badger.DB) {
+			defer thr.Done(nil)
+
+			mapFiles := filenamesInTree(dirs[shardId])
+			var mapItrs []*mapIterator
+			for _, mapFile := range mapFiles {
+				itr := newMapIterator(mapFile)
+				mapItrs = append(mapItrs, itr)
+			}
+
+			writer := db.NewStreamWriter()
+			if err := writer.Prepare(); err != nil {
+				x.Check(err)
+			}
+
+			ci := &countIndexer{reducer: r, writer: writer}
+			r.reduce(mapItrs, ci)
+			ci.wait()
+
+			if err := writer.Flush(); err != nil {
+				x.Check(err)
+			}
+			for _, itr := range mapItrs {
+				if err := itr.Close(); err != nil {
+					fmt.Printf("Error while closing iterator: %v", err)
+				}
+			}
+		}(i, r.createBadger(i))
 	}
-	thr.Wait()
-	r.writesThr.Wait()
+	return thr.Finish()
 }
 
-func (r *reducer) reduce(job shuffleOutput) {
+func (r *reducer) createBadger(i int) *badger.DB {
+	opt := badger.DefaultOptions
+	opt.SyncWrites = false
+	opt.TableLoadingMode = bo.MemoryMap
+	opt.ValueThreshold = 1 << 10 // 1 KB.
+	opt.Dir = r.opt.shardOutputDirs[i]
+	opt.ValueDir = opt.Dir
+	opt.Logger = nil
+	db, err := badger.OpenManaged(opt)
+	x.Check(err)
+	r.dbs = append(r.dbs, db)
+	return db
+}
+
+type mapIterator struct {
+	fd     *os.File
+	reader *bufio.Reader
+	tmpBuf []byte
+}
+
+func (mi *mapIterator) Close() error {
+	return mi.fd.Close()
+}
+
+func (mi *mapIterator) Next() *pb.MapEntry {
+	r := mi.reader
+	buf, err := r.Peek(binary.MaxVarintLen64)
+	if err == io.EOF {
+		return nil
+	}
+	x.Check(err)
+	sz, n := binary.Uvarint(buf)
+	if n <= 0 {
+		log.Fatalf("Could not read uvarint: %d", n)
+	}
+	x.Check2(r.Discard(n))
+
+	for cap(mi.tmpBuf) < int(sz) {
+		mi.tmpBuf = make([]byte, sz)
+	}
+	x.Check2(io.ReadFull(r, mi.tmpBuf[:sz]))
+
+	me := new(pb.MapEntry)
+	x.Check(proto.Unmarshal(mi.tmpBuf[:sz], me))
+	return me
+}
+
+func newMapIterator(filename string) *mapIterator {
+	fd, err := os.Open(filename)
+	x.Check(err)
+
+	return &mapIterator{fd: fd, reader: bufio.NewReaderSize(fd, 16<<10)}
+}
+
+func (r *reducer) encodeAndWrite(
+	writer *badger.StreamWriter, entryCh chan []*pb.MapEntry, closer *y.Closer) {
+	defer closer.Done()
+
+	preds := make(map[string]uint32)
+
+	var listSize int
+	list := &bpb.KVList{}
+	for batch := range entryCh {
+		listSize += r.toList(batch, list)
+		if listSize > 4<<20 {
+			for _, kv := range list.Kv {
+				pk := x.Parse(kv.Key)
+				if len(pk.Attr) == 0 {
+					continue
+				}
+				streamId := preds[pk.Attr]
+				if streamId == 0 {
+					streamId = atomic.AddUint32(&r.streamId, 1)
+					preds[pk.Attr] = streamId
+				}
+				// TODO: Having many stream ids can cause memory issues with StreamWriter. So, we
+				// should build a way in StreamWriter to indicate that the stream is over, so the
+				// table for that stream can be flushed and memory released.
+				kv.StreamId = streamId
+			}
+			x.Check(writer.Write(list))
+			list = &bpb.KVList{}
+			listSize = 0
+		}
+	}
+	if len(list.Kv) > 0 {
+		x.Check(writer.Write(list))
+	}
+}
+
+func (r *reducer) reduce(mapItrs []*mapIterator, ci *countIndexer) {
+	entryCh := make(chan []*pb.MapEntry, 100)
+	closer := y.NewCloser(1)
+	defer closer.SignalAndWait()
+
+	var ph postingHeap
+	for _, itr := range mapItrs {
+		me := itr.Next()
+		if me != nil {
+			heap.Push(&ph, heapNode{mapEntry: me, itr: itr})
+		} else {
+			fmt.Printf("NIL first map entry for %s", itr.fd.Name())
+		}
+	}
+
+	writer := ci.writer
+	go r.encodeAndWrite(writer, entryCh, closer)
+
+	const batchSize = 10000
+	const batchAlloc = batchSize * 11 / 10
+	batch := make([]*pb.MapEntry, 0, batchAlloc)
+	var prevKey []byte
+	var plistLen int
+
+	for len(ph.nodes) > 0 {
+		node0 := &ph.nodes[0]
+		me := node0.mapEntry
+		node0.mapEntry = node0.itr.Next()
+		if node0.mapEntry != nil {
+			heap.Fix(&ph, 0)
+		} else {
+			heap.Pop(&ph)
+		}
+
+		keyChanged := !bytes.Equal(prevKey, me.Key)
+		// Note that the keys are coming in sorted order from the heap. So, if
+		// we see a new key, we should push out the number of entries we got
+		// for the current key, so the count index can register that.
+		if keyChanged && plistLen > 0 {
+			ci.addUid(prevKey, plistLen)
+			plistLen = 0
+		}
+
+		if len(batch) >= batchSize && keyChanged {
+			entryCh <- batch
+			batch = make([]*pb.MapEntry, 0, batchAlloc)
+		}
+		prevKey = me.Key
+		batch = append(batch, me)
+		plistLen++
+	}
+	if len(batch) > 0 {
+		entryCh <- batch
+	}
+	if plistLen > 0 {
+		ci.addUid(prevKey, plistLen)
+	}
+	close(entryCh)
+}
+
+type heapNode struct {
+	mapEntry *pb.MapEntry
+	itr      *mapIterator
+}
+
+type postingHeap struct {
+	nodes []heapNode
+}
+
+func (h *postingHeap) Len() int {
+	return len(h.nodes)
+}
+func (h *postingHeap) Less(i, j int) bool {
+	return less(h.nodes[i].mapEntry, h.nodes[j].mapEntry)
+}
+func (h *postingHeap) Swap(i, j int) {
+	h.nodes[i], h.nodes[j] = h.nodes[j], h.nodes[i]
+}
+func (h *postingHeap) Push(x interface{}) {
+	h.nodes = append(h.nodes, x.(heapNode))
+}
+func (h *postingHeap) Pop() interface{} {
+	elem := h.nodes[len(h.nodes)-1]
+	h.nodes = h.nodes[:len(h.nodes)-1]
+	return elem
+}
+
+func (r *reducer) toList(mapEntries []*pb.MapEntry, list *bpb.KVList) int {
 	var currentKey []byte
 	var uids []uint64
 	pl := new(pb.PostingList)
-	txn := job.db.NewTransactionAt(r.state.writeTs, true)
+	var size int
 
-	outputPostingList := func() {
+	appendToList := func() {
 		atomic.AddInt64(&r.prog.reduceKeyCount, 1)
 
 		// For a UID-only posting list, the badger value is a delta packed UID
@@ -65,25 +276,29 @@ func (r *reducer) reduce(job shuffleOutput) {
 		// list when the value is read by dgraph.  For a value posting list,
 		// the full pb.Posting type is used (which pb.y contains the
 		// delta packed UID list).
-		meta := posting.BitCompletePosting
+		if len(uids) == 0 {
+			return
+		}
 		pl.Pack = codec.Encode(uids, 256)
 		val, err := pl.Marshal()
 		x.Check(err)
-		x.Check(txn.SetEntry(&badger.Entry{
-			Key:      currentKey,
+		kv := &bpb.KV{
+			Key:      y.Copy(currentKey),
 			Value:    val,
-			UserMeta: meta,
-		}))
-
+			UserMeta: []byte{posting.BitCompletePosting},
+			Version:  r.state.writeTs,
+		}
+		size += kv.Size()
+		list.Kv = append(list.Kv, kv)
 		uids = uids[:0]
 		pl.Reset()
 	}
 
-	for _, mapEntry := range job.mapEntries {
+	for _, mapEntry := range mapEntries {
 		atomic.AddInt64(&r.prog.reduceEdgeCount, 1)
 
 		if !bytes.Equal(mapEntry.Key, currentKey) && currentKey != nil {
-			outputPostingList()
+			appendToList()
 		}
 		currentKey = mapEntry.Key
 
@@ -99,12 +314,6 @@ func (r *reducer) reduce(job shuffleOutput) {
 			pl.Postings = append(pl.Postings, mapEntry.Posting)
 		}
 	}
-	outputPostingList()
-
-	NumBadgerWrites.Add(1)
-	x.Check(txn.CommitAt(r.state.writeTs, func(err error) {
-		x.Check(err)
-		NumBadgerWrites.Add(-1)
-		r.writesThr.Done()
-	}))
+	appendToList()
+	return size
 }

dgraph/cmd/bulk/run.go
@@ -71,8 +71,8 @@ func init() {
 	flag.Bool("cleanup_tmp", true,
 		"Clean up the tmp directory after the loader finishes. Setting this to false allows the"+
 			" bulk loader can be re-run while skipping the map phase.")
-	flag.Int("shufflers", 1,
-		"Number of shufflers to run concurrently. Increasing this can improve performance, and "+
+	flag.Int("reducers", 1,
+		"Number of reducers to run concurrently. Increasing this can improve performance, and "+
 			"must be less than or equal to the number of reduce shards.")
 	flag.Bool("version", false, "Prints the version of Dgraph Bulk Loader.")
 	flag.BoolP("store_xids", "x", false, "Generate an xid edge for each node.")
@@ -107,7 +107,7 @@ func run() {
 		MapBufSize:       int64(Bulk.Conf.GetInt("mapoutput_mb")),
 		SkipMapPhase:     Bulk.Conf.GetBool("skip_map_phase"),
 		CleanupTmp:       Bulk.Conf.GetBool("cleanup_tmp"),
-		NumShufflers:     Bulk.Conf.GetInt("shufflers"),
+		NumReducers:      Bulk.Conf.GetInt("reducers"),
 		Version:          Bulk.Conf.GetBool("version"),
 		StoreXids:        Bulk.Conf.GetBool("store_xids"),
 		ZeroAddr:         Bulk.Conf.GetString("zero"),
@@ -142,9 +142,9 @@ func run() {
 			opt.ReduceShards, opt.MapShards)
 		os.Exit(1)
 	}
-	if opt.NumShufflers > opt.ReduceShards {
+	if opt.NumReducers > opt.ReduceShards {
 		fmt.Fprintf(os.Stderr, "Invalid flags: shufflers(%d) should be <= reduce_shards(%d)\n",
-			opt.NumShufflers, opt.ReduceShards)
+			opt.NumReducers, opt.ReduceShards)
 		os.Exit(1)
 	}
 	if opt.CustomTokenizers != "" {

dgraph/cmd/bulk/shuffle.go
@@ -1,180 +1,0 @@
-/*
- * Copyright 2017-2018 Dgraph Labs, Inc. and Contributors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package bulk
-
-import (
-	"bufio"
-	"bytes"
-	"container/heap"
-	"encoding/binary"
-	"io"
-	"log"
-	"os"
-
-	"github.com/dgraph-io/badger"
-	bo "github.com/dgraph-io/badger/options"
-	"github.com/dgraph-io/dgraph/protos/pb"
-	"github.com/dgraph-io/dgraph/x"
-	"github.com/gogo/protobuf/proto"
-)
-
-type shuffler struct {
-	*state
-	output chan<- shuffleOutput
-}
-
-func (s *shuffler) run() {
-	shardDirs := shardDirs(s.opt.TmpDir)
-	x.AssertTrue(len(shardDirs) == s.opt.ReduceShards)
-	x.AssertTrue(len(s.opt.shardOutputDirs) == s.opt.ReduceShards)
-
-	thr := x.NewThrottle(s.opt.NumShufflers)
-	for i := 0; i < s.opt.ReduceShards; i++ {
-		thr.Start()
-		go func(shardId int, db *badger.DB) {
-			mapFiles := filenamesInTree(shardDirs[shardId])
-			shuffleInputChs := make([]chan *pb.MapEntry, len(mapFiles))
-			for i, mapFile := range mapFiles {
-				shuffleInputChs[i] = make(chan *pb.MapEntry, 1000)
-				go readMapOutput(mapFile, shuffleInputChs[i])
-			}
-
-			ci := &countIndexer{state: s.state, db: db}
-			s.shufflePostings(shuffleInputChs, ci)
-			ci.wait()
-			thr.Done()
-		}(i, s.createBadger(i))
-	}
-	thr.Wait()
-	close(s.output)
-}
-
-func (s *shuffler) createBadger(i int) *badger.DB {
-	opt := badger.DefaultOptions
-	opt.SyncWrites = false
-	opt.TableLoadingMode = bo.MemoryMap
-	opt.Dir = s.opt.shardOutputDirs[i]
-	opt.ValueDir = opt.Dir
-	db, err := badger.OpenManaged(opt)
-	x.Check(err)
-	s.dbs = append(s.dbs, db)
-	return db
-}
-
-func readMapOutput(filename string, mapEntryCh chan<- *pb.MapEntry) {
-	fd, err := os.Open(filename)
-	x.Check(err)
-	defer fd.Close()
-	r := bufio.NewReaderSize(fd, 16<<10)
-
-	unmarshalBuf := make([]byte, 1<<10)
-	for {
-		buf, err := r.Peek(binary.MaxVarintLen64)
-		if err == io.EOF {
-			break
-		}
-		x.Check(err)
-		sz, n := binary.Uvarint(buf)
-		if n <= 0 {
-			log.Fatalf("Could not read uvarint: %d", n)
-		}
-		x.Check2(r.Discard(n))
-
-		for cap(unmarshalBuf) < int(sz) {
-			unmarshalBuf = make([]byte, sz)
-		}
-		x.Check2(io.ReadFull(r, unmarshalBuf[:sz]))
-
-		me := new(pb.MapEntry)
-		x.Check(proto.Unmarshal(unmarshalBuf[:sz], me))
-		mapEntryCh <- me
-	}
-
-	close(mapEntryCh)
-}
-
-func (s *shuffler) shufflePostings(mapEntryChs []chan *pb.MapEntry, ci *countIndexer) {
-	var ph postingHeap
-	for _, ch := range mapEntryChs {
-		heap.Push(&ph, heapNode{mapEntry: <-ch, ch: ch})
-	}
-
-	const batchSize = 1000
-	const batchAlloc = batchSize * 11 / 10
-	batch := make([]*pb.MapEntry, 0, batchAlloc)
-	var prevKey []byte
-	var plistLen int
-	for len(ph.nodes) > 0 {
-		me := ph.nodes[0].mapEntry
-		var ok bool
-		ph.nodes[0].mapEntry, ok = <-ph.nodes[0].ch
-		if ok {
-			heap.Fix(&ph, 0)
-		} else {
-			heap.Pop(&ph)
-		}
-
-		keyChanged := !bytes.Equal(prevKey, me.Key)
-		if keyChanged && plistLen > 0 {
-			ci.addUid(prevKey, plistLen)
-			plistLen = 0
-		}
-
-		if len(batch) >= batchSize && !bytes.Equal(prevKey, me.Key) {
-			s.output <- shuffleOutput{mapEntries: batch, db: ci.db}
-			NumQueuedReduceJobs.Add(1)
-			batch = make([]*pb.MapEntry, 0, batchAlloc)
-		}
-		prevKey = me.Key
-		batch = append(batch, me)
-		plistLen++
-	}
-	if len(batch) > 0 {
-		s.output <- shuffleOutput{mapEntries: batch, db: ci.db}
-		NumQueuedReduceJobs.Add(1)
-	}
-	if plistLen > 0 {
-		ci.addUid(prevKey, plistLen)
-	}
-}
-
-type heapNode struct {
-	mapEntry *pb.MapEntry
-	ch       <-chan *pb.MapEntry
-}
-
-type postingHeap struct {
-	nodes []heapNode
-}
-
-func (h *postingHeap) Len() int {
-	return len(h.nodes)
-}
-func (h *postingHeap) Less(i, j int) bool {
-	return less(h.nodes[i].mapEntry, h.nodes[j].mapEntry)
-}
-func (h *postingHeap) Swap(i, j int) {
-	h.nodes[i], h.nodes[j] = h.nodes[j], h.nodes[i]
-}
-func (h *postingHeap) Push(x interface{}) {
-	h.nodes = append(h.nodes, x.(heapNode))
-}
-func (h *postingHeap) Pop() interface{} {
-	elem := h.nodes[len(h.nodes)-1]
-	h.nodes = h.nodes[:len(h.nodes)-1]
-	return elem
-}

dgraph/cmd/debug/wal.go
@@ -130,7 +130,7 @@ func printRaft(db *badger.DB, store *raftwal.DiskStorage) {
 				if err != nil {
 					log.Fatalf("Unable to marshal entry: %+v. Error: %v", ent, err)
 				}
-				if err := batch.Set(k, data, 0); err != nil {
+				if err := batch.Set(k, data); err != nil {
 					log.Fatalf("Unable to set data: %+v", err)
 				}
 			default:

raftwal/storage.go
@@ -342,7 +342,7 @@ func (w *DiskStorage) setSnapshot(batch *badger.WriteBatch, s raftpb.Snapshot) e
 	if err != nil {
 		return errors.Wrapf(err, "wal.Store: While marshal snapshot")
 	}
-	if err := batch.Set(w.snapshotKey(), data, 0); err != nil {
+	if err := batch.Set(w.snapshotKey(), data); err != nil {
 		return err
 	}
 
@@ -351,7 +351,7 @@ func (w *DiskStorage) setSnapshot(batch *badger.WriteBatch, s raftpb.Snapshot) e
 	if err != nil {
 		return err
 	}
-	if err := batch.Set(w.EntryKey(e.Index), data, 0); err != nil {
+	if err := batch.Set(w.EntryKey(e.Index), data); err != nil {
 		return err
 	}
 
@@ -378,7 +378,7 @@ func (w *DiskStorage) setHardState(batch *badger.WriteBatch, st raftpb.HardState
 	if err != nil {
 		return errors.Wrapf(err, "wal.Store: While marshal hardstate")
 	}
-	return batch.Set(w.HardStateKey(), data, 0)
+	return batch.Set(w.HardStateKey(), data)
 }
 
 // reset resets the entries. Used for testing.
@@ -399,7 +399,7 @@ func (w *DiskStorage) reset(es []raftpb.Entry) error {
 			return errors.Wrapf(err, "wal.Store: While marshal entry")
 		}
 		k := w.EntryKey(e.Index)
-		if err := batch.Set(k, data, 0); err != nil {
+		if err := batch.Set(k, data); err != nil {
 			return err
 		}
 	}
@@ -662,7 +662,7 @@ func (w *DiskStorage) addEntries(batch *badger.WriteBatch, entries []raftpb.Entr
 		if err != nil {
 			return errors.Wrapf(err, "wal.Append: While marshal entry")
 		}
-		if err := batch.Set(k, data, 0); err != nil {
+		if err := batch.Set(k, data); err != nil {
 			return err
 		}
 	}

commit 0045988e4b1d545a2adb6b1ff2cc68e637843e7f

Set UserMeta in bulk loader. (#3649)
Bug fix for bulk loader changes introduced in #3542.

Fixes #3607.

Signed-off-by: பாலாஜி ஜின்னா <balaji@dgraph.io>

dgraph/cmd/bulk/count_index.go
@@ -89,7 +89,7 @@ func (c *countIndexer) writeIndex(pred string, rev bool, counts map[int][]uint64
 		list.Kv = append(list.Kv, &bpb.KV{
 			Key:      x.CountKey(pred, uint32(count), rev),
 			Value:    data,
-			Meta:     []byte{posting.BitCompletePosting},
+			UserMeta: []byte{posting.BitCompletePosting},
 			Version:  c.state.writeTs,
 			StreamId: streamId,
 		})