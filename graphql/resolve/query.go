jsLover1117/dgraph

commit b4535f6f5925cc224c366b22c22b3483b0edbf35

perf(GraphQL): Generate GraphQL query response by optimized JSON encoding (GraphQL-730) (#7371)
* perf(GraphQL): JSON Part-1: JSON is generated by Dgraph for simple queries (GRAPHQL-877) (#7230)

This PR moves the GraphQL JSON generation for simple queries inside Dgraph.

Previously, the GraphQL layer would get a JSON response from Dgraph in the form expected for DQL. That JSON response had to go through the following pipeline:

Unmarshal
Run GraphQL completion
Marshal
After this pipeline was executed, then the output JSON would be in the form as expected by GraphQL.

Now, the response from Dgraph would already be in the form expected by GraphQL. So, we no more need to run the above pipeline. Hence, saving valuable time and memory.

* perf(GraphQL): JSON Part-2: JSON is generated by Dgraph for simple mutations (GRAPHQL-877) (#7237)

This PR is a continuation of #7230.

It makes standard GraphQL mutations work with the JSON changes.

Each mutation now returns the GraphQL JSON required for that mutation as a byte array from the MutationResolver itself, so no further processing of the JSON is required.
Delete mutations now query the `queryField` explicitly before executing the mutation, to make it work with the JSON changes.

* perf(GraphQL): JSON Part-3: Geo queries (GRAPHQL-879) (#7238)

Continued from:
* #7230
* #7237

This PR makes Geo queries/mutations work with the new JSON changes.

* perf(GraphQL): JSON Part-4: Aggregate queries at root and child levels (GRAPHQL-936) (#7240)

Continued from:
* #7230
* #7237
* #7238

This PR makes aggregate queries at root and child levels work with the new JSON changes.
It removes the default behaviour of rewriting the count field to DQL query everytime (introduced in #7119). Now, rewritten DQL will have count field only if it was requested in GraphQL.

* review comments from merged PRs

graphql/resolve/query.go
@@ -55,17 +55,15 @@ func (qr QueryResolverFunc) Resolve(ctx context.Context, query schema.Query) *Re
 
 // NewQueryResolver creates a new query resolver.  The resolver runs the pipeline:
 // 1) rewrite the query using qr (return error if failed)
-// 2) execute the rewritten query with qe (return error if failed)
-// 3) process the result with rc
-func NewQueryResolver(qr QueryRewriter, ex DgraphExecutor, rc ResultCompleter) QueryResolver {
-	return &queryResolver{queryRewriter: qr, executor: ex, resultCompleter: rc}
+// 2) execute the rewritten query with ex (return error if failed)
+func NewQueryResolver(qr QueryRewriter, ex DgraphExecutor) QueryResolver {
+	return &queryResolver{queryRewriter: qr, executor: ex}
 }
 
 // a queryResolver can resolve a single GraphQL query field.
 type queryResolver struct {
-	queryRewriter   QueryRewriter
-	executor        DgraphExecutor
-	resultCompleter ResultCompleter
+	queryRewriter QueryRewriter
+	executor      DgraphExecutor
 }
 
 func (qr *queryResolver) Resolve(ctx context.Context, query schema.Query) *Resolved {
@@ -84,11 +82,6 @@ func (qr *queryResolver) Resolve(ctx context.Context, query schema.Query) *Resol
 	defer timer.Stop()
 
 	resolved := qr.rewriteAndExecute(ctx, query)
-	if resolved.Data == nil {
-		resolved.Data = map[string]interface{}{query.Name(): nil}
-	}
-
-	qr.resultCompleter.Complete(ctx, resolved)
 	resolverTrace.Dgraph = resolved.Extensions.Tracing.Execution.Resolvers[0].Dgraph
 	resolved.Extensions.Tracing.Execution.Resolvers[0] = resolverTrace
 	return resolved
@@ -108,9 +101,13 @@ func (qr *queryResolver) rewriteAndExecute(ctx context.Context, query schema.Que
 
 	emptyResult := func(err error) *Resolved {
 		return &Resolved{
-			Data:       map[string]interface{}{query.DgraphAlias(): nil},
+			// all the auto-generated queries are nullable, but users may define queries with
+			// @custom(dql: ...) which may be non-nullable. So, we need to set the Data field
+			// only if the query was nullable and keep it nil if it was non-nullable.
+			// query.NullResponse() method handles that.
+			Data:       query.NullResponse(),
 			Field:      query,
-			Err:        err,
+			Err:        schema.SetPathIfEmpty(err, query.ResponseName()),
 			Extensions: ext,
 		}
 	}
@@ -144,34 +141,32 @@ func (qr *queryResolver) rewriteAndExecute(ctx context.Context, query schema.Que
 
 	queryTimer := newtimer(ctx, &dgraphQueryDuration.OffsetDuration)
 	queryTimer.Start()
-	resp, err := qr.executor.Execute(ctx, &dgoapi.Request{Query: qry, Vars: vars, ReadOnly: true})
+	resp, err := qr.executor.Execute(ctx, &dgoapi.Request{Query: qry, Vars: vars,
+		ReadOnly: true}, query)
 	queryTimer.Stop()
 
-	if err != nil {
+	if err != nil && !x.IsGqlErrorList(err) {
+		err = schema.GQLWrapf(err, "Dgraph query failed")
 		glog.Infof("Dgraph query execution failed : %s", err)
-		return emptyResult(schema.GQLWrapf(err, "Dgraph query failed"))
 	}
 
 	ext.TouchedUids = resp.GetMetrics().GetNumUids()[touchedUidsKey]
-	resolved := completeDgraphResult(ctx, query, resp.GetJson(), err)
-	resolved.Extensions = ext
+	resolved := &Resolved{
+		Data:       resp.GetJson(),
+		Field:      query,
+		Err:        schema.SetPathIfEmpty(err, query.ResponseName()),
+		Extensions: ext,
+	}
 
 	return resolved
 }
 
 func resolveIntrospection(ctx context.Context, q schema.Query) *Resolved {
 	data, err := schema.Introspect(q)
-
-	var result map[string]interface{}
-	var err2 error
-	if len(data) > 0 {
-		err2 = json.Unmarshal(data, &result)
-	}
-
 	return &Resolved{
-		Data:  result,
+		Data:  data,
 		Field: q,
-		Err:   schema.AppendGQLErrs(err, err2),
+		Err:   err,
 	}
 }
 

graphql/e2e/custom_logic/custom_logic_test.go
@@ -446,8 +446,7 @@ func TestCustomQueryWithNonExistentURLShouldReturnError(t *testing.T) {
 	require.Equal(t, x.GqlErrorList{
 		{
 			Message: "Evaluation of custom field failed because external request returned an " +
-				"error: unexpected error with: 404 for field: myFavoriteMovies within" +
-				" type: Query.",
+				"error: unexpected error with: 404 for field: myFavoriteMovies within type: Query.",
 			Locations: []x.Location{{Line: 3, Column: 3}},
 		},
 	}, result.Errors)
@@ -530,10 +529,14 @@ func TestCustomQueryShouldPropagateErrorFromFields(t *testing.T) {
 	expectedErrors := x.GqlErrorList{
 		&x.GqlError{Message: "Evaluation of custom field failed because external request " +
 			"returned an error: unexpected error with: 404 for field: cars within type: Person.",
-			Locations: []x.Location{{Line: 6, Column: 4}}},
+			Locations: []x.Location{{Line: 6, Column: 4}},
+			Path:      []interface{}{"queryPerson"},
+		},
 		&x.GqlError{Message: "Evaluation of custom field failed because external request returned" +
 			" an error: unexpected error with: 404 for field: bikes within type: Person.",
-			Locations: []x.Location{{Line: 9, Column: 4}}},
+			Locations: []x.Location{{Line: 9, Column: 4}},
+			Path:      []interface{}{"queryPerson"},
+		},
 	}
 	require.Contains(t, result.Errors, expectedErrors[0])
 	require.Contains(t, result.Errors, expectedErrors[1])
@@ -1223,7 +1226,7 @@ func TestCustomFieldResolutionShouldPropagateGraphQLErrors(t *testing.T) {
 	sort.Slice(result.Errors, func(i, j int) bool {
 		return result.Errors[i].Message < result.Errors[j].Message
 	})
-	require.Equal(t, x.GqlErrorList{
+	expectedErrs := x.GqlErrorList{
 		{
 			Message: "error-1 from cars",
 		},
@@ -1248,7 +1251,11 @@ func TestCustomFieldResolutionShouldPropagateGraphQLErrors(t *testing.T) {
 		{
 			Message: "error-2 from username",
 		},
-	}, result.Errors)
+	}
+	for _, err := range expectedErrs {
+		err.Path = []interface{}{"queryUser"}
+	}
+	require.Equal(t, expectedErrs, result.Errors)
 
 	expected := `{
 		"queryUser": [
@@ -1543,7 +1550,15 @@ func TestCustomLogicWithErrorResponse(t *testing.T) {
 
 	result := params.ExecuteAsPost(t, common.GraphqlURL)
 	require.Equal(t, `{"getCountriesErr":[]}`, string(result.Data))
-	require.Equal(t, "dummy error", result.Errors.Error())
+	require.Equal(t, x.GqlErrorList{
+		&x.GqlError{Message: "dummy error"},
+		&x.GqlError{
+			Message: "Evaluation of custom field failed because key: country could not be found " +
+				"in the JSON response returned by external request for field: getCountriesErr" +
+				" within type: Query.",
+			Locations: []x.Location{{Line: 3, Column: 3}},
+		},
+	}, result.Errors)
 }
 
 type episode struct {
@@ -1748,6 +1763,11 @@ func TestCustomFieldsWithXidShouldBeResolved(t *testing.T) {
 	common.RequireNoGQLErrors(t, result)
 	testutil.CompareJSON(t, expected, string(result.Data))
 
+	// cleanup
+	common.DeleteGqlType(t, "Episode", common.GetXidFilter("name", []interface{}{"episode-1",
+		"episode-2", "episode-3"}), 3, nil)
+	common.DeleteGqlType(t, "Character", common.GetXidFilter("name", []interface{}{"character-1",
+		"character-2", "character-3"}), 3, nil)
 }
 
 func TestCustomPostMutation(t *testing.T) {
@@ -2550,6 +2570,7 @@ func TestRestCustomLogicInDeepNestedField(t *testing.T) {
 }
 
 func TestCustomDQL(t *testing.T) {
+	t.Skipf("enable after fixing @custom(dql: ...)")
 	dg, err := testutil.DgraphClient(common.Alpha1gRPC)
 	require.NoError(t, err)
 	testutil.DropAll(t, dg)
@@ -2847,6 +2868,7 @@ func TestCustomFieldsWithRestError(t *testing.T) {
 	require.Equal(t, x.GqlErrorList{
 		{
 			Message: "Rest API returns Error for field name",
+			Path:    []interface{}{"queryUser"},
 		},
 	}, result.Errors)
 
@@ -2920,29 +2942,24 @@ func TestCustomResolverInInterfaceImplFrag(t *testing.T) {
 			method: "POST",
 			body: "{name: $name, totalCredits: $totalCredits}"
 		})
+	}
+	type Droid implements Character {
+		primaryFunction: String
 	}`
 	common.SafelyUpdateGQLSchemaOnAlpha1(t, schema)
 
 	addCharacterParams := &common.GraphQLParams{
 		Query: `mutation {
 			addHuman(input: [{name: "Han", totalCredits: 10}]) {
-				human {
-					id
-			  	}
+				numUids
+			}
+			addDroid(input: [{name: "R2-D2", primaryFunction: "Robot"}]) {
+				numUids
 			}
 		}`,
 	}
 	resp := addCharacterParams.ExecuteAsPost(t, common.GraphqlURL)
 	common.RequireNoGQLErrors(t, resp)
-
-	var addResp struct {
-		AddHuman struct {
-			Human []struct {
-				ID string
-			}
-		}
-	}
-	require.NoError(t, json.Unmarshal(resp.Data, &addResp))
 
 	queryCharacterParams := &common.GraphQLParams{
 		Query: `query {
@@ -2950,6 +2967,9 @@ func TestCustomResolverInInterfaceImplFrag(t *testing.T) {
 				name
 				... on Human {
 					bio
+				}
+				... on Droid {
+					primaryFunction
 				}
 			}
 		}`,
@@ -2962,14 +2982,147 @@ func TestCustomResolverInInterfaceImplFrag(t *testing.T) {
 		{
 		  "name": "Han",
 		  "bio": "My name is Han and I have 10 credits."
+		}, {
+		  "name": "R2-D2",
+		  "primaryFunction": "Robot"
 		}
 	  ]
 	}`, string(resp.Data))
 
 	// cleanup
-	common.DeleteGqlType(t, "Character", map[string]interface{}{"id": []interface{}{addResp.
-		AddHuman.Human[0].ID}},
-		1, nil)
+	common.DeleteGqlType(t, "Character", common.GetXidFilter("name", []interface{}{"Han",
+		"R2-D2"}), 2, nil)
+}
+
+// See: https://discuss.dgraph.io/t/custom-field-resolvers-are-not-always-called/12489
+func TestCustomFieldIsResolvedWhenNoModeGiven(t *testing.T) {
+	sch := `
+	type ItemType {
+	  typeId: String! @id
+	  name: String
+
+	  marketStats: MarketStatsR @custom(http: {
+		url: "http://localhost:8080/graphql", # We are using the same alpha to serve MarketStats.
+		method: POST,
+		graphql: "query($typeId:String!) { getMarketStats(typeId: $typeId) }",
+		skipIntrospection: true,
+	  })
+	}
+	
+	type Blueprint {
+	  blueprintId: String! @id
+	  shallowProducts: [ItemType]
+	  deepProducts: [BlueprintProduct]
+	}
+	
+	type BlueprintProduct {
+	  itemType: ItemType
+	  amount: Int
+	}
+	
+	type MarketStats  {
+	  typeId: String! @id
+	  price: Float 
+	}
+	
+	type MarketStatsR @remote {
+	  typeId: String
+	  price: Float
+	}`
+	common.SafelyUpdateGQLSchemaOnAlpha1(t, sch)
+
+	mutation := &common.GraphQLParams{
+		Query: `mutation AddExampleData {
+		  addItemType(input: {
+			typeId: "1"
+			name: "Test"
+		  }) { numUids }
+		  addMarketStats(input: {
+			typeId: "1"
+			price: 9.99
+		  }) { numUids }
+		  addBlueprint(input: {
+			blueprintId: "bp1"
+			shallowProducts: [{ typeId: "1" }]
+			deepProducts: [{
+			  amount: 1
+			  itemType: { typeId: "1" }
+			}]
+		  }) { numUids }
+		}`,
+	}
+	resp := mutation.ExecuteAsPost(t, common.GraphqlURL)
+	common.RequireNoGQLErrors(t, resp)
+
+	query := &common.GraphQLParams{
+		Query: `query {
+		  works: getItemType(typeId:"1") {
+			typeId
+			marketStats { price }
+		  }
+		  doesntWork: getItemType(typeId: "1") {
+			marketStats { price }
+		  }
+		  shallowWorks: getBlueprint(blueprintId:"bp1") {
+			shallowProducts {
+			  typeId
+			  marketStats { price }
+			}
+		  }
+		  deepDoesntWork: getBlueprint(blueprintId:"bp1") {
+			deepProducts {
+			  itemType {
+				typeId
+				marketStats { price }
+			  }
+			}
+		  }
+		}`,
+	}
+	resp = query.ExecuteAsPost(t, common.GraphqlURL)
+	common.RequireNoGQLErrors(t, resp)
+
+	testutil.CompareJSON(t, `{
+	  "works": {
+		"marketStats": {
+		  "price": 9.99
+		},
+		"typeId": "1"
+	  },
+	  "doesntWork": {
+		"marketStats": {
+		  "price": 9.99
+		}
+	  },
+	  "shallowWorks": {
+		"shallowProducts": [
+		  {
+			"marketStats": {
+			  "price": 9.99
+			},
+			"typeId": "1"
+		  }
+		]
+	  },
+	  "deepDoesntWork": {
+		"deepProducts": [
+		  {
+			"itemType": {
+			  "marketStats": {
+			    "price": 9.99
+			  },
+			  "typeId": "1"
+			}
+		  }
+		]
+	  }
+	}`, string(resp.Data))
+
+	// cleanup
+	common.DeleteGqlType(t, "ItemType", map[string]interface{}{}, 1, nil)
+	common.DeleteGqlType(t, "Blueprint", map[string]interface{}{}, 1, nil)
+	common.DeleteGqlType(t, "BlueprintProduct", map[string]interface{}{}, 1, nil)
+	common.DeleteGqlType(t, "MarketStats", map[string]interface{}{}, 1, nil)
 }
 
 func TestMain(m *testing.M) {

graphql/e2e/common/query.go
@@ -17,6 +17,7 @@
 package common
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -27,6 +28,10 @@ import (
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/dgraph-io/dgo/v200"
+	"github.com/dgraph-io/dgo/v200/protos/api"
+	"google.golang.org/grpc"
 
 	"github.com/spf13/cast"
 
@@ -88,6 +93,7 @@ func touchedUidsHeader(t *testing.T) {
 	// confirm that the header value is a non-negative integer
 	touchedUidsInHeader, err := strconv.ParseUint(resp.Header.Get("Graphql-TouchedUids"), 10, 64)
 	require.NoError(t, err)
+	require.Greater(t, touchedUidsInHeader, uint64(0))
 
 	// confirm that the value in header is same as the value in body
 	var gqlResp GraphQLResponse
@@ -1340,6 +1346,18 @@ func includeAndSkipDirective(t *testing.T) {
 				title
 				tags
 			  }
+			  postsAggregate {
+				__typename @include(if: $includeFalse) @skip(if: $skipFalse)
+				count @include(if: $includeFalse) @skip(if: $skipTrue)
+				titleMin @include(if: $includeTrue) @skip(if: $skipFalse)
+				numLikesMax @include(if: $includeTrue) @skip(if: $skipTrue)
+			  }
+			}
+			aggregatePost {
+			  __typename @include(if: $includeFalse) @skip(if: $skipFalse)
+			  count @include(if: $includeFalse) @skip(if: $skipTrue)
+			  titleMin @include(if: $includeTrue) @skip(if: $skipFalse)
+			  numLikesMax @include(if: $includeTrue) @skip(if: $skipTrue)
 			}
 		  }`,
 		Variables: map[string]interface{}{
@@ -1353,7 +1371,19 @@ func includeAndSkipDirective(t *testing.T) {
 	gqlResponse := getAuthorParams.ExecuteAsPost(t, GraphqlURL)
 	RequireNoGQLErrors(t, gqlResponse)
 
-	expected := `{"queryAuthor":[{"name":"Ann Other Author"}]}`
+	expected := `{
+	  "queryAuthor": [
+		{
+		  "name": "Ann Other Author",
+		  "postsAggregate": {
+			"titleMin": "Learning GraphQL in Dgraph"
+		  }
+		}
+	  ],
+	  "aggregatePost": {
+		"titleMin": "GraphQL doco"
+	  }
+	}`
 	require.JSONEq(t, expected, string(gqlResponse.Data))
 }
 
@@ -2001,6 +2031,64 @@ func queriesHaveExtensions(t *testing.T) {
 	require.Greater(t, int(gqlResponse.Extensions[touchedUidskey].(float64)), 0)
 }
 
+func erroredQueriesHaveTouchedUids(t *testing.T) {
+	country1 := addCountry(t, postExecutor)
+	country2 := addCountry(t, postExecutor)
+
+	// delete the first country's name.
+	// The schema states type Country `{ ... name: String! ... }`
+	// so a query error will be raised if we ask for the country's name in a
+	// query.  Don't think a GraphQL update can do this ATM, so do through Dgraph.
+	d, err := grpc.Dial(Alpha1gRPC, grpc.WithInsecure())
+	require.NoError(t, err)
+	client := dgo.NewDgraphClient(api.NewDgraphClient(d))
+	mu := &api.Mutation{
+		CommitNow: true,
+		DelNquads: []byte(fmt.Sprintf("<%s> <Country.name> * .", country1.ID)),
+	}
+	_, err = client.NewTxn().Mutate(context.Background(), mu)
+	require.NoError(t, err)
+
+	// query country's name with some other things, that should give us error for missing name.
+	query := &GraphQLParams{
+		Query: `query ($ids: [ID!]) {
+			queryCountry(filter: {id: $ids}) {
+				id
+				name
+			}
+		}`,
+		Variables: map[string]interface{}{"ids": []interface{}{country1.ID, country2.ID}},
+	}
+	gqlResponse := query.ExecuteAsPost(t, GraphqlURL)
+
+	// the data should have first country as null
+	expectedResponse := fmt.Sprintf(`{
+		"queryCountry": [
+			null,
+			{"id": "%s", "name": "Testland"}
+		]
+	}`, country2.ID)
+	testutil.CompareJSON(t, expectedResponse, string(gqlResponse.Data))
+
+	// we should also get error for the missing name field
+	require.Equal(t, x.GqlErrorList{{
+		Message: "Non-nullable field 'name' (type String!) was not present " +
+			"in result from Dgraph.  GraphQL error propagation triggered.",
+		Locations: []x.Location{{Line: 4, Column: 5}},
+		Path:      []interface{}{"queryCountry", float64(0), "name"},
+	}}, gqlResponse.Errors)
+
+	// response should have extensions
+	require.NotNil(t, gqlResponse.Extensions)
+	// it should have touched_uids filled in from Dgraph response's metrics
+	touchedUidskey := "touched_uids"
+	require.Contains(t, gqlResponse.Extensions, touchedUidskey)
+	require.Greater(t, int(gqlResponse.Extensions[touchedUidskey].(float64)), 0)
+
+	// cleanup
+	deleteCountry(t, map[string]interface{}{"id": []interface{}{country1.ID, country2.ID}}, 2, nil)
+}
+
 func queryWithAlias(t *testing.T) {
 	queryPostParams := &GraphQLParams{
 		Query: `query {
@@ -2439,7 +2527,7 @@ func queryWithCascade(t *testing.T) {
 	deleteAuthors(t, authorIds, nil)
 	deleteCountry(t, map[string]interface{}{"id": countryIds}, len(countryIds), nil)
 	DeleteGqlType(t, "Post", map[string]interface{}{"postID": postIds}, len(postIds), nil)
-	deleteState(t, getXidFilter("xcode", []string{states[0].Code, states[1].Code}), len(states),
+	deleteState(t, GetXidFilter("xcode", []interface{}{states[0].Code, states[1].Code}), len(states),
 		nil)
 	cleanupStarwars(t, newStarship.ID, humanID, "")
 }
@@ -2825,7 +2913,7 @@ func filterInQueriesWithArrayForAndOr(t *testing.T) {
 	deleteAuthors(t, authorIds, nil)
 	deleteCountry(t, map[string]interface{}{"id": countryIds}, len(countryIds), nil)
 	DeleteGqlType(t, "Post", map[string]interface{}{"postID": postIds}, len(postIds), nil)
-	deleteState(t, getXidFilter("xcode", []string{states[0].Code, states[1].Code}), len(states),
+	deleteState(t, GetXidFilter("xcode", []interface{}{states[0].Code, states[1].Code}), len(states),
 		nil)
 	cleanupStarwars(t, newStarship.ID, humanID, "")
 }
@@ -2972,6 +3060,7 @@ func queryAggregateOnEmptyData(t *testing.T) {
 			aggregatePost (filter: {title : { anyofterms : "Nothing" }} ) {
 				count
 				numLikesMax
+				type: __typename
 				titleMin
 			}
 		}`,
@@ -2979,10 +3068,15 @@ func queryAggregateOnEmptyData(t *testing.T) {
 
 	gqlResponse := queryPostParams.ExecuteAsPost(t, GraphqlURL)
 	RequireNoGQLErrors(t, gqlResponse)
-	testutil.CompareJSON(t,
+	require.JSONEq(t,
 		`{
-					"aggregatePost": null
-				}`,
+			"aggregatePost": {
+				"count": 0,
+				"numLikesMax": null,
+				"type": "PostAggregateResult",
+				"titleMin": null
+			}
+		}`,
 		string(gqlResponse.Data))
 }
 
@@ -3151,6 +3245,7 @@ func queryAggregateAtChildLevel(t *testing.T) {
 				name
 				ag : statesAggregate {
 					count
+					__typename
 					nameMin
 				}
 			}
@@ -3165,6 +3260,7 @@ func queryAggregateAtChildLevel(t *testing.T) {
 				"name": "India",
 				"ag": { 
 					"count" : 3,
+					"__typename": "StateAggregateResult",
 					"nameMin": "Gujarat"
 				}
 			}]
@@ -3209,8 +3305,10 @@ func queryAggregateAtChildLevelWithEmptyData(t *testing.T) {
 				name
 				ag : statesAggregate(filter: {xcode: {in: ["nothing"]}}) {
                 	count
+					__typename
 					nameMin
                 }
+				n: name
 			}
 		}`,
 	}
@@ -3221,7 +3319,12 @@ func queryAggregateAtChildLevelWithEmptyData(t *testing.T) {
 		{
 			"queryCountry": [{
 				"name": "India",
-				"ag": null
+				"ag": {
+					"count": 0,
+					"__typename": "StateAggregateResult",
+					"nameMin": null
+				},
+				"n": "India"
 			}]
 		}`,
 		string(gqlResponse.Data))
@@ -3699,7 +3802,6 @@ func idDirectiveWithInt(t *testing.T) {
 	query := &GraphQLParams{
 		Query: `query {
 		  getChapter(chapterId: 1) {
-			bookId
 			chapterId
 			name
 		  }
@@ -3710,7 +3812,6 @@ func idDirectiveWithInt(t *testing.T) {
 	RequireNoGQLErrors(t, response)
 	var expected = `{
 	  	"getChapter": {
-			"bookId": 1234567890,
 			"chapterId": 1,
 			"name": "How Dgraph Works"
 		  }

commit 05b1ae76bb2ad2203f71fb2f420c4f19616aafc7

fix(GraphQL): fix custom DQL (GRAPHQL-1020) (#7449)
This PR makes `@custom(dql: ...)` work again after the GraphQL response JSON generation changes went in and broke it in #7371.

graphql/resolve/query.go
@@ -112,37 +112,16 @@ func (qr *queryResolver) rewriteAndExecute(ctx context.Context, query schema.Que
 		}
 	}
 
-	var qry string
-	vars := make(map[string]string)
-
-	// DQL queries don't need any rewriting, as they are already in DQL form
-	if query.QueryType() == schema.DQLQuery {
-		qry = query.DQLQuery()
-		args := query.Arguments()
-		for k, v := range args {
-			// dgoapi.Request{}.Vars accepts only string values for variables,
-			// so need to convert all variable values to string
-			vStr, err := convertScalarToString(v)
-			if err != nil {
-				return emptyResult(schema.GQLWrapf(err, "couldn't convert argument %s to string",
-					k))
-			}
-			// the keys in dgoapi.Request{}.Vars are assumed to be prefixed with $
-			vars["$"+k] = vStr
-		}
-	} else {
-		dgQuery, err := qr.queryRewriter.Rewrite(ctx, query)
-		if err != nil {
-			return emptyResult(schema.GQLWrapf(err, "couldn't rewrite query %s",
-				query.ResponseName()))
-		}
-		qry = dgraph.AsString(dgQuery)
+	dgQuery, err := qr.queryRewriter.Rewrite(ctx, query)
+	if err != nil {
+		return emptyResult(schema.GQLWrapf(err, "couldn't rewrite query %s",
+			query.ResponseName()))
 	}
+	qry := dgraph.AsString(dgQuery)
 
 	queryTimer := newtimer(ctx, &dgraphQueryDuration.OffsetDuration)
 	queryTimer.Start()
-	resp, err := qr.executor.Execute(ctx, &dgoapi.Request{Query: qry, Vars: vars,
-		ReadOnly: true}, query)
+	resp, err := qr.executor.Execute(ctx, &dgoapi.Request{Query: qry, ReadOnly: true}, query)
 	queryTimer.Stop()
 
 	if err != nil && !x.IsGqlErrorList(err) {
@@ -158,6 +137,89 @@ func (qr *queryResolver) rewriteAndExecute(ctx context.Context, query schema.Que
 		Extensions: ext,
 	}
 
+	return resolved
+}
+
+func NewCustomDQLQueryResolver(ex DgraphExecutor) QueryResolver {
+	return &customDQLQueryResolver{executor: ex}
+}
+
+type customDQLQueryResolver struct {
+	executor DgraphExecutor
+}
+
+func (qr *customDQLQueryResolver) Resolve(ctx context.Context, query schema.Query) *Resolved {
+	span := otrace.FromContext(ctx)
+	stop := x.SpanTimer(span, "resolveCustomDQLQuery")
+	defer stop()
+
+	resolverTrace := &schema.ResolverTrace{
+		Path:       []interface{}{query.ResponseName()},
+		ParentType: "Query",
+		FieldName:  query.ResponseName(),
+		ReturnType: query.Type().String(),
+	}
+	timer := newtimer(ctx, &resolverTrace.OffsetDuration)
+	timer.Start()
+	defer timer.Stop()
+
+	resolved := qr.rewriteAndExecute(ctx, query)
+	resolverTrace.Dgraph = resolved.Extensions.Tracing.Execution.Resolvers[0].Dgraph
+	resolved.Extensions.Tracing.Execution.Resolvers[0] = resolverTrace
+	return resolved
+}
+
+func (qr *customDQLQueryResolver) rewriteAndExecute(ctx context.Context,
+	query schema.Query) *Resolved {
+	dgraphQueryDuration := &schema.LabeledOffsetDuration{Label: "query"}
+	ext := &schema.Extensions{
+		Tracing: &schema.Trace{
+			Execution: &schema.ExecutionTrace{
+				Resolvers: []*schema.ResolverTrace{
+					{Dgraph: []*schema.LabeledOffsetDuration{dgraphQueryDuration}},
+				},
+			},
+		},
+	}
+
+	emptyResult := func(err error) *Resolved {
+		resolved := EmptyResult(query, err)
+		resolved.Extensions = ext
+		return resolved
+	}
+
+	dgQuery := query.DQLQuery()
+	args := query.Arguments()
+	vars := make(map[string]string)
+	for k, v := range args {
+		// dgoapi.Request{}.Vars accepts only string values for variables,
+		// so need to convert all variable values to string
+		vStr, err := convertScalarToString(v)
+		if err != nil {
+			return emptyResult(schema.GQLWrapf(err, "couldn't convert argument %s to string", k))
+		}
+		// the keys in dgoapi.Request{}.Vars are assumed to be prefixed with $
+		vars["$"+k] = vStr
+	}
+
+	queryTimer := newtimer(ctx, &dgraphQueryDuration.OffsetDuration)
+	queryTimer.Start()
+	resp, err := qr.executor.Execute(ctx, &dgoapi.Request{Query: dgQuery, Vars: vars,
+		ReadOnly: true}, nil)
+	queryTimer.Stop()
+
+	if err != nil {
+		return emptyResult(schema.GQLWrapf(err, "Dgraph query failed"))
+	}
+	ext.TouchedUids = resp.GetMetrics().GetNumUids()[touchedUidsKey]
+
+	var respJson map[string]interface{}
+	if err = schema.Unmarshal(resp.Json, &respJson); err != nil {
+		return emptyResult(schema.GQLWrapf(err, "couldn't unmarshal Dgraph result"))
+	}
+
+	resolved := DataResult(query, respJson, nil)
+	resolved.Extensions = ext
 	return resolved
 }
 

graphql/resolve/resolver.go
@@ -236,7 +236,7 @@ func (rf *resolverFactory) WithConventionResolvers(
 	for _, q := range s.Queries(schema.DQLQuery) {
 		rf.WithQueryResolver(q, func(q schema.Query) QueryResolver {
 			// DQL queries don't need any QueryRewriter
-			return NewQueryResolver(nil, fns.Ex)
+			return NewCustomDQLQueryResolver(fns.Ex)
 		})
 	}
 

graphql/e2e/custom_logic/custom_logic_test.go
@@ -2569,7 +2569,6 @@ func TestRestCustomLogicInDeepNestedField(t *testing.T) {
 }
 
 func TestCustomDQL(t *testing.T) {
-	t.Skipf("enable after fixing @custom(dql: ...)")
 	common.SafelyDropAll(t)
 
 	schema := `